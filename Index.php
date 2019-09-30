e4d0e3f066bfc3c15acd2368b186b0f52fe3179d
Rodrigo Perez <r.igin@live.com.mx>
2019-09-29 20:29:33 -0500

    Evidencia 2.0

---
+++ Index.php
@@ -0,0 +1,426 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <title>Rodrigo Perez</title>
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+</head>
+<body>
+
+    <a id="whatsapp" href="https://api.whatsapp.com/send?phone=528115131393"><img src="img/whatsapp.png" alt="whatsapp"></a>
+    
+    <header>
+
+        <div class="menu">
+
+            <div class="contenedor">
+
+                <p class="logo">RODRIGO PEREZ</p>
+                <img class = "menu-icon" src="img/menu.png" alt="menu">
+
+                <nav>
+                    
+                    <ul class="lista-menu">
+
+                        <li><a href="#sobre-mi">Sobre mi</a></li>
+
+                        <li><a href="#servicios">Servicios</a></li>
+
+                        <li><a href="#mis-trabajos">Mis Trabajos</a></li>
+
+                        <li><a href="#por-que-trabajar">¿Porque trabajar conmigo?</a></li>
+                        
+                        <li><a href="#blog">Blog</a></li>
+                        
+                        <li><a href="#contacto">Contactos</a></li>
+
+                    </ul>
+                
+                </nav>
+            
+            </div>
+
+        </div>
+
+        <div class="contenedor" id="contenedor-titulo-flex">
+            
+            <div class="contenedor-titulo">
+                
+                <h1>Rodrigo Perez</h1>
+
+                <h2>Diseñador & Desarrollador Web</h2>
+
+                <a href="">Ver Trabajos</a>
+            
+            </div>
+
+        </div>
+    
+    </header>
+
+    <main>
+
+        <section id="sobre-mi">
+
+            <div class="contenedor">
+
+                <h3>Sobre Mi</h3>
+
+                <div class="contenedor-sobre-mi">
+
+                    <div class="computadoras">
+                        
+                        <img src="img/computadoras.png" alt="computadoras">
+
+                    </div>
+                        
+                    <div class="texto">
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Dolorem voluptas eaque nemo obcaecati beatae, quas dicta dolore saepe, 
+                        inventore omnis ducimus quo aliquam tempora aliquid, 
+                        porro ipsa temporibus facilis asperiores?</p>
+                        
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Dolorem voluptas eaque nemo obcaecati beatae, quas dicta dolore saepe, 
+                        inventore omnis ducimus quo aliquam tempora aliquid, 
+                        porro ipsa temporibus facilis asperiores?</p>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Dolorem voluptas eaque nemo obcaecati beatae, quas dicta dolore saepe, 
+                        inventore omnis ducimus quo aliquam tempora aliquid, 
+                        porro ipsa temporibus facilis asperiores?</p>
+
+                        <a href="">VER MIS TRABAJOS</a>
+
+                    </div>
+
+                </div>
+            
+            </div>
+
+        </section>
+
+        <section id="servicios">
+
+            <div class="contenedor">
+
+                <h3>SERVICIOS</h3>
+
+                <div class="contenedor-servicios">
+
+                    <div class="servicio violeta">
+
+                        <h4>Diseño Web</h4>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Ut id temporibus praesentium eligendi. Totam quos quod inventore eum nulla nisi 
+                        tenetur necessitatibus unde ut quo magnam porro, voluptatum voluptatibus aliquam!</p>
+        
+                        <img class="icono" src="img/monitor.png" alt="monitor">
+                        <img class="ondas" src="img/waves.png" alt="fondo">
+
+                    </div>
+
+                    <div class="servicio celeste">
+
+                        <h4>Responsive Design</h4>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Ut id temporibus praesentium eligendi. Totam quos quod inventore eum nulla nisi 
+                        tenetur necessitatibus unde ut quo magnam porro, voluptatum voluptatibus aliquam!</p>
+        
+                        <img class="icono" src="img/mobile.png" alt="mobile">
+                        <img class="ondas" src="img/waves.png" alt="fondo">
+
+                    </div>
+
+                    <div class="servicio violeta">
+
+                        <h4>SEO</h4>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Ut id temporibus praesentium eligendi. Totam quos quod inventore eum nulla nisi 
+                        tenetur necessitatibus unde ut quo magnam porro, voluptatum voluptatibus aliquam!</p>
+        
+                        <img class="icono" src="img/seo.png" alt="seo">
+                        <img class="ondas" src="img/waves.png" alt="fondo">
+
+                    </div>
+
+                    <div class="servicio celeste">
+
+                        <h4>Autoadministrable</h4>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Ut id temporibus praesentium eligendi. Totam quos quod inventore eum nulla nisi 
+                        tenetur necessitatibus unde ut quo magnam porro, voluptatum voluptatibus aliquam!</p>
+        
+                        <img class="icono" src="img/wordpress.png" alt="wordpress">
+                        <img class="ondas" src="img/waves.png" alt="fondo">
+
+                    </div>
+
+                    <div class="servicio violeta">
+
+                        <h4>Mantenimiento</h4>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Ut id temporibus praesentium eligendi. Totam quos quod inventore eum nulla nisi 
+                        tenetur necessitatibus unde ut quo magnam porro, voluptatum voluptatibus aliquam!</p>
+        
+                        <img class="icono" src="img/mante.png" alt="mantenimiento">
+                        <img class="ondas" src="img/waves.png" alt="fondo">
+
+                    </div>
+
+                    <div class="servicio celeste">
+
+                        <h4>Marketing Digital</h4>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Ut id temporibus praesentium eligendi. Totam quos quod inventore eum nulla nisi 
+                        tenetur necessitatibus unde ut quo magnam porro, voluptatum voluptatibus aliquam!</p>
+        
+                        <img class="icono" src="img/marketing.png" alt="marketing">
+                        <img class="ondas" src="img/waves.png" alt="fondo">
+
+                    </div>
+
+                </div>
+
+            </div>
+            
+        </section>
+
+        <section id="mis-trabajos">
+
+            <div class="contenedor">
+                <h3>MIS TRABAJOS</h3>
+                <div class="owl-carousel owl-theme">
+                    
+                    <div class="item"><a href="#"></a><img src="img/1.jpg" alt=""></div>
+                    <div class="item"><a href="#"></a><img src="img/2.jpg" alt=""></div>
+                    <div class="item"><a href="#"></a><img src="img/3.jpg" alt=""></div>
+                    <div class="item"><a href="#"></a><img src="img/4.jpg" alt=""></div>
+                    <div class="item"><a href="#"></a><img src="img/1.jpg" alt=""></div>
+                    <div class="item"><a href="#"></a><img src="img/2.jpg" alt=""></div>
+                    <div class="item"><a href="#"></a><img src="img/3.jpg" alt=""></div>
+                    <div class="item"><a href="#"></a><img src="img/4.jpg" alt=""></div>
+                    <div class="item"><a href="#"></a><img src="img/1.jpg" alt=""></div>
+                    <div class="item"><a href="#"></a><img src="img/2.jpg" alt=""></div>
+                    <div class="item"><a href="#"></a><img src="img/3.jpg" alt=""></div>
+                    <div class="item"><a href="#"></a><img src="img/4.jpg" alt=""></div>
+                </div>
+
+            </div>
+
+        </section>
+
+        <section id="por-que-trabajar">
+
+            <div class="contenedor">
+                
+                <h3>¿Porque Trabajar Conmigo?</h3>
+
+                <div class="contenedor-trabajar">
+
+                    <div class="item">
+
+                        <ul>
+                            
+                            <li><img src="img/velocidad.png" alt="velocidad"></li>
+                            <li>Velocidad</li>
+                            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                                Sunt cupiditate iusto fuga, harum nihil consequatur praesentium magni? 
+                                Molestiae odit blanditiis fugiat, harum adipisci neque omnis ab doloremque obcaecati quia sed.</li>
+                        
+                        </ul>
+
+                    </div>
+
+                    <div class="item">
+
+                        <ul>
+                                
+                            <li><img src="img/mantenimiento.png" alt="mantenimiento"></li>
+                            <li>Velocidad</li>
+                            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                                Sunt cupiditate iusto fuga, harum nihil consequatur praesentium magni? 
+                                Molestiae odit blanditiis fugiat, harum adipisci neque omnis ab doloremque obcaecati quia sed.</li>
+                            
+                        </ul>
+    
+                    </div>
+
+                    <div class="item">
+
+                        <ul>
+
+                            <li><img src="img/mobile2.png" alt="mobile"></li>
+                            <li>Velocidad</li>
+                            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                                Sunt cupiditate iusto fuga, harum nihil consequatur praesentium magni? 
+                                Molestiae odit blanditiis fugiat, harum adipisci neque omnis ab doloremque obcaecati quia sed.</li>
+                            
+                        </ul>
+    
+                    </div>
+
+                </div>
+                            
+            </div>
+
+        </section>
+
+        <section id="blog">
+
+            <div class="contenedor"></div>
+
+            <h3>Blog</h3>
+
+            <div class="contenedor-publicaciones">
+
+                <div class="publicacion">
+
+                    <img src="img/blog1.jpg" alt="blog">
+                    
+                    <div class="contenido">
+
+                        <h4>No alcanza con tener una sola web</h4>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Nihil iusto eius soluta explicabo dicta expedita facilis 
+                        recusandae deleniti inventore corporis adipisci, totam, 
+                        odio nulla iure sint laudantium blanditiis! Debitis, ullam.
+                        <br><br>
+                        Nihil iusto eius soluta explicabo dicta expedita facilis 
+                        odio nulla iure sint laudantium blanditiis! Debitis, ullam.
+                        <br><br>
+                        Nihil iusto eius soluta explicabo dicta expedita facilis 
+                        odio nulla iure sint laudantium blanditiis! Debitis, ullam.</p>
+                            
+                        <a href="#">Leer más →</a>
+                    
+                    </div>
+
+                </div>
+
+                <div class="publicacion">
+
+                    <img src="img/blog2.jpg" alt="blog">
+
+                    <div class="contenido">
+
+                        <h4>No alcanza con tener una sola web</h4>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Nihil iusto eius soluta explicabo dicta expedita facilis 
+                        recusandae deleniti inventore corporis adipisci, totam, 
+                        odio nulla iure sint laudantium blanditiis! Debitis, ullam.
+                        <br><br>
+                        Nihil iusto eius soluta explicabo dicta expedita facilis 
+                        odio nulla iure sint laudantium blanditiis! Debitis, ullam.
+                        <br><br>
+                        Nihil iusto eius soluta explicabo dicta expedita facilis 
+                        odio nulla iure sint laudantium blanditiis! Debitis, ullam.</p>
+        
+                        <a href="#">Leer más →</a>
+                
+                    </div>            
+                
+                </div>
+            
+            </div>
+
+        </section>
+
+        <section id="que-esperas">
+
+            <div class="contenedor">
+
+                <h3>¿Que esperas para tener tu pagina web?</h3>
+                <a href="#">Charlemos</a>
+            </div>
+
+        </section>
+
+        <section id="contacto">
+            <?php
+            
+                if(@$_GET['i']=='ok') { // QUIERE DECIR QUE EL FORMULARIO SE ENVIO CORRECTAMENTE ?>
+
+                <h3>La consulta se envio correctamente. Nos contactaremos a la brevedad.</h3>
+
+            <?php
+
+                } else{
+            ?>
+            
+            <div class="contenedor">
+            
+                <h3>Contacto</h3>
+
+                <form action="envio-formulario.php" method="POST" enctype="multipart/form-data">
+
+                    <input type="text" placeholder="Nombre" name="nombre" required>
+                    <input type="email" placeholder="Email" name="email" required>
+                    <textarea type="text" placeholder="Mensaje" name="mensaje" required></textarea>
+                    <input type="submit" value="Enviar Mensaje">
+                </form>
+
+                <?php } ?>
+
+                <div class="contacto-info">
+
+                    <div class="mail"><img src="img/mail-icon.png" alt="">rodrigoperezvaz97@gmail.com</div>
+                    <div class="whatsapp"><img src="img/whatsapp-icon.png" alt="">52+ 8115131393</div>
+                    
+                </div>
+
+            </div>
+        
+        </section>
+    
+    </main>
+
+    <footer>
+
+        <div class="contenedor">
+            
+            <div class="redes">
+                    
+                <a href="#"><img src="img/facebook.png" alt="facebook"></a>
+                <a href="#"><img src="img/github.png" alt="github"></a>
+                <a href="#"><img src="img/linkedin.png" alt="linkedin"></a>
+                <a href="#"><img src="img/medium.png" alt="medium"></a>
+
+            </div>
+            
+            <div class="parrafo">
+
+            <p>© 2019 Rodrigo Perez | Diseñador Web México. Todos los derechos reservados</p>
+
+            </div>
+
+        </div>
+
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/owl.js"></script>
+    <script src="js/scroll.js"></script>
+</body>
+</html>
